db29777a8f8ab000ac601ce125d0a36b34162da7
Ashley Bland <ashleyjbland@gmail.com>
2018-08-17 22:11:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] != " " || board[index] != ""
     false
-  else (board[index] == " " || board[index] == "") or (board[index] == "X" || board[index] == "0")
+  else
     true
   end
 end
