0dc723e6f42f80538c63b6adb106eb26c4e7b535
Ashley Bland <ashleyjbland@gmail.com>
2018-08-17 21:22:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,8 @@
 def position_taken?(board, index)
   if board[index] != "  " || " "
     false
+  elsif board[index] does not equal ("  " || " ")
+    true
   else board[index] || ("  " || " ")
     true
   end
